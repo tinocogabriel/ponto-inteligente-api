06f0b719f6e08cdcbd628a3f326ee611e670efd6
Gabriel Tinoco <tinocogabriel@live.com>
2021-07-05 15:59:09 -0300

    Primeiro commit

---
+++ src/test/java/com/aulaspringboot/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulaspringboot.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
